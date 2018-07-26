066a8bcb1915c8dd14ed295124f53ece7914f858
Shaozhen Ding <dsz0111@gmail.com>
2018-07-25 21:16:55 -0600

    add intermidiate cert

--- certificate/fixtures/generate_keys.sh
+++ certificate/fixtures/generate_keys.sh
@@ -3,10 +3,10 @@
 openssl req -x509 -config root.conf -nodes -newkey rsa:2048 -keyout root.key -out root.crt -days 365
 
 #generate intermidiate csr
-openssl req -config intermidiate.conf -nodes -new -newkey rsa:2048 -out intermidiate.csr -keyout intermidiate.key
+ openssl req -config intermidiate.conf -nodes -new -newkey rsa:2048 -out intermidiate.csr -keyout intermidiate.key
 
 #Generate intermidiate crt
-openssl x509 -req -days 365 -in intermidiate.csr -extfile intermidiate.conf -CA root.crt -CAkey root.key -CAcreateserial -out intermidiate.crt -extensions v3_req
+ openssl x509 -req -days 365 -in intermidiate.csr -extfile intermidiate.conf -CA root.crt -CAkey root.key -CAcreateserial -out intermidiate.crt -extensions v3_req
 
 #Generate end csr
 openssl req -config user.conf -nodes -new -newkey rsa:2048 -out user.csr -keyout user.key
